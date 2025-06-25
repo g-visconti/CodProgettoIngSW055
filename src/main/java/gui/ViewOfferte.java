78bd635727e93465156434364b31e40bec2c7b44
g-visconti <g.visconti.na@gmail.com>
2025-06-25 16:37:37 +0200

    Update ViewOfferte.java
    La finestra ora è ridimensionabile

--- src/main/java/gui/ViewOfferte.java
+++ src/main/java/gui/ViewOfferte.java
@@ -21,7 +21,6 @@ public class ViewOfferte extends JFrame {
 	 * Create the frame ViewOfferte.
 	 */
 	public ViewOfferte(String emailUtente) {
-		setResizable(false);
 		// Carica l'immagine come icona
 		URL pathIcona = getClass().getClassLoader().getResource("images/DietiEstatesIcona.png");
 		if (pathIcona != null) {
@@ -34,72 +33,91 @@ public class ViewOfferte extends JFrame {
 
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(null);
 		setContentPane(contentPane);
+		SpringLayout sl_contentPane = new SpringLayout();
+		contentPane.setLayout(sl_contentPane);
 
 		// PANNELLO COMANDI
 		JPanel panelComandi = new JPanel();
-		panelComandi.setBackground(Color.WHITE);
-		panelComandi.setBounds(0, 0, 1232, 134);
-		panelComandi.setLayout(null);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, panelComandi, 0, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.WEST, panelComandi, 0, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, panelComandi, 134, SpringLayout.NORTH, contentPane);
+		panelComandi.setBackground(new Color(255, 255, 255));
 		contentPane.add(panelComandi);
+		SpringLayout sl_panelComandi = new SpringLayout();
+		panelComandi.setLayout(sl_panelComandi);
 
 		JLabel lblTitolo = new JLabel("DietiEstates25");
-		lblTitolo.setHorizontalAlignment(SwingConstants.LEFT);
+		sl_panelComandi.putConstraint(SpringLayout.NORTH, lblTitolo, 47, SpringLayout.NORTH, panelComandi);
+		sl_panelComandi.putConstraint(SpringLayout.WEST, lblTitolo, 489, SpringLayout.WEST, panelComandi);
+		sl_panelComandi.putConstraint(SpringLayout.EAST, lblTitolo, -490, SpringLayout.EAST, panelComandi);
+		lblTitolo.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTitolo.setForeground(new Color(27, 99, 142));
 		lblTitolo.setFont(new Font("Tahoma", Font.BOLD, 30));
-		lblTitolo.setBounds(516, 11, 225, 29);
 		panelComandi.add(lblTitolo);
 
-		JLabel lblDEicona = new JLabel();
-		URL pathDEicona = getClass().getClassLoader().getResource("images/DietiEstatesicona.png");
-		if (pathDEicona != null) {
-			lblDEicona.setIcon(new ImageIcon(pathDEicona));
-		}
-		lblDEicona.setBounds(481, 11, 28, 29);
-		panelComandi.add(lblDEicona);
 
 		JLabel lblDescrizione = new JLabel("Seleziona un immobile per visualizzare le offerte ricevute");
+		sl_panelComandi.putConstraint(SpringLayout.SOUTH, lblTitolo, -13, SpringLayout.NORTH, lblDescrizione);
+		sl_panelComandi.putConstraint(SpringLayout.NORTH, lblDescrizione, 97, SpringLayout.NORTH, panelComandi);
+		sl_panelComandi.putConstraint(SpringLayout.SOUTH, lblDescrizione, -8, SpringLayout.SOUTH, panelComandi);
+		sl_panelComandi.putConstraint(SpringLayout.WEST, lblDescrizione, 220, SpringLayout.WEST, panelComandi);
+		sl_panelComandi.putConstraint(SpringLayout.EAST, lblDescrizione, -220, SpringLayout.EAST, panelComandi);
 		lblDescrizione.setHorizontalAlignment(SwingConstants.CENTER);
 		lblDescrizione.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblDescrizione.setBounds(224, 69, 784, 29);
 		panelComandi.add(lblDescrizione);
 
 		URL pathBack = getClass().getClassLoader().getResource("images/Back.png");
 		ImageIcon iconaBack = new ImageIcon(pathBack);
 		JButton btnBack = new JButton(iconaBack);
+		sl_panelComandi.putConstraint(SpringLayout.NORTH, btnBack, 11, SpringLayout.NORTH, panelComandi);
+		sl_panelComandi.putConstraint(SpringLayout.WEST, btnBack, 10, SpringLayout.WEST, panelComandi);
+		sl_panelComandi.putConstraint(SpringLayout.SOUTH, btnBack, 39, SpringLayout.NORTH, panelComandi);
+		sl_panelComandi.putConstraint(SpringLayout.EAST, btnBack, 38, SpringLayout.WEST, panelComandi);
 		btnBack.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
 			}
 		});
-		btnBack.setBounds(10, 11, 28, 28);
 		btnBack.setToolTipText("Clicca qui per tornare alla dashboard");
 		btnBack.setBorderPainted(false);
 		panelComandi.add(btnBack);
 		
 		JLabel lblBenvenuto = new JLabel("Accesso effettuato con:");
+		sl_panelComandi.putConstraint(SpringLayout.NORTH, lblBenvenuto, 0, SpringLayout.NORTH, btnBack);
+		sl_panelComandi.putConstraint(SpringLayout.WEST, lblBenvenuto, 951, SpringLayout.EAST, btnBack);
+		sl_panelComandi.putConstraint(SpringLayout.EAST, lblBenvenuto, -10, SpringLayout.EAST, panelComandi);
 		lblBenvenuto.setVerticalAlignment(SwingConstants.TOP);
 		lblBenvenuto.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblBenvenuto.setBounds(997, 11, 225, 14);
 		panelComandi.add(lblBenvenuto);
 
 		JLabel lblEmailAccesso = new JLabel(emailUtente + " ");
+		sl_panelComandi.putConstraint(SpringLayout.SOUTH, lblEmailAccesso, -52, SpringLayout.NORTH, lblDescrizione);
+		sl_panelComandi.putConstraint(SpringLayout.NORTH, lblEmailAccesso, 6, SpringLayout.SOUTH, lblBenvenuto);
+		sl_panelComandi.putConstraint(SpringLayout.WEST, lblEmailAccesso, 951, SpringLayout.EAST, btnBack);
+		sl_panelComandi.putConstraint(SpringLayout.EAST, lblEmailAccesso, -10, SpringLayout.EAST, panelComandi);
 		lblEmailAccesso.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblEmailAccesso.setFont(new Font("Yu Gothic UI Semibold", Font.ITALIC, 11));
-		lblEmailAccesso.setBounds(997, 26, 225, 23);
 		panelComandi.add(lblEmailAccesso);
 
 		// TABELLA RISULTATI
 		JPanel panelRisultati = new JPanel();
+		sl_contentPane.putConstraint(SpringLayout.EAST, panelComandi, 0, SpringLayout.EAST, panelRisultati);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, panelRisultati, 134, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.WEST, panelRisultati, 0, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, panelRisultati, 0, SpringLayout.SOUTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, panelRisultati, 0, SpringLayout.EAST, contentPane);
 		panelRisultati.setBackground(new Color(50, 133, 177));
-		panelRisultati.setBounds(0, 134, 1232, 773);
-		panelRisultati.setLayout(null);
 		contentPane.add(panelRisultati);
+		SpringLayout sl_panelRisultati = new SpringLayout();
+		panelRisultati.setLayout(sl_panelRisultati);
 
 		JScrollPane scrollPane = new JScrollPane();
+		sl_panelRisultati.putConstraint(SpringLayout.NORTH, scrollPane, 18, SpringLayout.NORTH, panelRisultati);
+		sl_panelRisultati.putConstraint(SpringLayout.WEST, scrollPane, 10, SpringLayout.WEST, panelRisultati);
+		sl_panelRisultati.putConstraint(SpringLayout.SOUTH, scrollPane, -10, SpringLayout.SOUTH, panelRisultati);
+		sl_panelRisultati.putConstraint(SpringLayout.EAST, scrollPane, -10, SpringLayout.EAST, panelRisultati);
 		scrollPane.setBackground(Color.WHITE);
-		scrollPane.setBounds(10, 35, 1212, 727);
 		panelRisultati.add(scrollPane);
 
 		tableRisultati = new JTable();
