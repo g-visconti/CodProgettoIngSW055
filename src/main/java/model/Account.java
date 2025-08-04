9de4c963634e4cdb23ceb445016563c92b9897d4
Manu2791 <borgiamanuele@gmail.com>
2025-08-04 11:34:07 +0200

    Classi Account e Cliente

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -1,18 +1,48 @@
 package model;
 
 public class Account {
-    
-    private String email;
+	 private String idAccount;
+	private String email;
     private String nome;
     private String cognome;
     private String password;
+    private String citta;
+    private String telefono;
+    private String cap;
+    private String indirizzo;
+    private String ruolo;
+    
+    
+    
+//Costruttore con tutti i parametri
+    public Account(String email, String password, String nome, String cognome, String citta, String telefono, String cap, String indirizzo, String ruolo) {
+        this.email = email;
+        this.password = password;
+        this.nome=nome;
+        this.setCitta(citta);
+        this.setTelefono(telefono);
+        this.setCap(cap);
+        this.setIndirizzo(indirizzo);
+        this.setRuolo(ruolo);
+        
+        
+     
+    }
 
-    // Costruttore
+    
+    // Costruttore con 2 parametri
     public Account(String email, String password) {
         this.email = email;
         this.password = password;
     }
 
+    public String getIdAccount() {
+        return idAccount;
+    }
+
+    public void setIdAccount(String idAccount) {
+        this.idAccount = idAccount;
+    }
     // Getter e setter
     public String getEmail() { return email; }
     public String getNome() { return nome; }
@@ -24,5 +54,55 @@ public class Account {
     public void setNome(String nome) { this.nome = nome; }
     public void setCognome(String cognome) { this.cognome = cognome; }
     public void setPassword(String password) { this.password = password; }
+
+
+	public String getCitta() {
+		return citta;
+	}
+
+
+	public void setCitta(String citta) {
+		this.citta = citta;
+	}
+
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+
+	public String getCap() {
+		return cap;
+	}
+
+
+	public void setCap(String cap) {
+		this.cap = cap;
+	}
+
+
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+
+
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+
+
+	public String getRuolo() {
+		return ruolo;
+	}
+
+
+	public void setRuolo(String ruolo) {
+		this.ruolo = ruolo;
+	}
 
 }
