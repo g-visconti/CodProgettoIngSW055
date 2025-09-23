1240ff7fb5ce25f4949d653931685b6cb422e648
Gaetano Visconti (N86002282) <gae.visconti@studenti.unina.it>
2025-09-23 15:52:07 +0200

    Update Controller.java

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -33,7 +33,6 @@ import util.TextAreaRenderer;
 
 public class Controller {
      
-	
 	public void registraNuovoUtente(String email, String password)  { //Registrazione terze parti
 	    try {
 	        Connection connAWS = ConnessioneDatabase.getInstance().getConnection();
@@ -61,8 +60,7 @@ public class Controller {
 	    } catch (SQLException e) {
 	        e.printStackTrace();
 	    }
-	}
-	
+	}	
 	
 	public void registraNuovoUtente(String email, String password, String nome, String cognome, String citta, String telefono, String cap, String indirizzo, String ruolo) {
 	    try {
@@ -94,11 +92,8 @@ public class Controller {
 	    }
 	}
 
-
-
-
-     // controlla se l'email è già presente nel database
-     public boolean checkUtente(String email) throws SQLException {
+ 	// controlla se l'email è già presente nel database
+ 	public boolean checkUtente(String email) throws SQLException {
     	 Connection connAWS = ConnessioneDatabase.getInstance().getConnection();
     	 AccountDAO accountDAO = new AccountDAO(connAWS);
     	 boolean resultCheck;
@@ -113,26 +108,23 @@ public class Controller {
          }
          
          return resultCheck;
-     }
+    }
+       
+ 	// controlla se nel database esiste una tupla con email e password passata come parametri
+ 	public boolean checkCredenziali(String email, String password) throws SQLException{
+		 Connection connAWS = ConnessioneDatabase.getInstance().getConnection();
+		 AccountDAO accountDAO = new AccountDAO(connAWS);
+		 boolean resultCheck = false;
+		 
+		 if (accountDAO.checkCredenziali(email,password)) {
+	         resultCheck = true;
+	     }
+	     
+		 
+	     return resultCheck;
+    }
      
-     
-     // controlla se nel database esiste una tupla con email e password passata come parametri
-     public boolean checkCredenziali(String email, String password) throws SQLException{
-    	 Connection connAWS = ConnessioneDatabase.getInstance().getConnection();
-    	 AccountDAO accountDAO = new AccountDAO(connAWS);
-    	 boolean resultCheck = false;
-    	 
-    	 if (accountDAO.checkCredenziali(email,password)) {
-             resultCheck = true;
-         }
-         
-    	 
-         return resultCheck;
-     }
-     
-     
-     
-     public boolean updatePassword(String emailAssociata, String pass, String confermaPass) throws SQLException {
+ 	public boolean updatePassword(String emailAssociata, String pass, String confermaPass) throws SQLException {
          Connection connAWS = ConnessioneDatabase.getInstance().getConnection();
             AccountDAO accountDAO = new AccountDAO(connAWS);
             boolean resultCheck = false;
@@ -144,8 +136,7 @@ public class Controller {
             return resultCheck;
     }
      
-
-     public String emailToId(String email) throws SQLException{
+ 	public String emailToId(String email) throws SQLException{
     	 Connection connAWS = ConnessioneDatabase.getInstance().getConnection();
     	 AccountDAO accountDAO = new AccountDAO(connAWS);
     	 String idResult = "undef";
@@ -157,199 +148,184 @@ public class Controller {
     	 return idResult;
      }
      
+ 	public boolean caricaImmobile(Immobile imm) {
+	    try {
+	        Connection connAWS = ConnessioneDatabase.getInstance().getConnection();
+	        ImmobileDAO immobileDao = new ImmobileDAO(connAWS);
+
+	        String tipo = imm.getTipologia().toLowerCase(); // tipo da campo "tipologia"
+
+	        switch (tipo) {
+	            case "affitto":
+	                immobileDao.caricaImmobileInAffitto((ImmobileInAffitto) imm);
+	                break;
+	            case "vendita":
+	                immobileDao.caricaImmobileInVendita((ImmobileInVendita) imm);
+	                break;
+	            default:
+	                throw new IllegalArgumentException("Tipologia non valida: " + tipo);
+	        }
+
+	        return true;
+	    } catch (SQLException | IllegalArgumentException | ClassCastException e) {
+	        e.printStackTrace();
+	        return false;
+	    }
+	}
      
-     
-     
+ 	public Immobile recuperaDettagli(long idImmobile) {
+	    try {
+	        Connection connAWS = ConnessioneDatabase.getInstance().getConnection();
+	        ImmobileDAO immobileDAO = new ImmobileDAO(connAWS);
+	        return immobileDAO.getImmobileById(idImmobile);
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	        return null;
+	    }
+	}
 
-     public boolean caricaImmobile(Immobile imm) {
-    	    try {
-    	        Connection connAWS = ConnessioneDatabase.getInstance().getConnection();
-    	        ImmobileDAO immobileDao = new ImmobileDAO(connAWS);
+ 	public int riempiTableRisultati(JTable tableRisultati, String campoPieno, String tipologia, Filtri filtri) {
+	    try {
+	        Connection connAWS = ConnessioneDatabase.getInstance().getConnection();
+	        ImmobileDAO immobileDAO = new ImmobileDAO(connAWS);
 
-    	        String tipo = imm.getTipologia().toLowerCase(); // tipo da campo "tipologia"
+	        NumberFormat format = NumberFormat.getNumberInstance(Locale.ITALY);
+	        format.setGroupingUsed(true);
+	        format.setMaximumFractionDigits(0);
+	        format.setMinimumFractionDigits(0);
 
-    	        switch (tipo) {
-    	            case "affitto":
-    	                immobileDao.caricaImmobileInAffitto((ImmobileInAffitto) imm);
-    	                break;
-    	            case "vendita":
-    	                immobileDao.caricaImmobileInVendita((ImmobileInVendita) imm);
-    	                break;
-    	            default:
-    	                throw new IllegalArgumentException("Tipologia non valida: " + tipo);
-    	        }
+	        String[] colonne = { "ID", "Immagini", "Titolo dell'annuncio", "Descrizione", tipologia.equals("Vendita") ? "Prezzo Totale" : "Prezzo Mensile" };
+	        @SuppressWarnings("serial")
+	        DefaultTableModel model = new DefaultTableModel(colonne, 0) {
+	            @Override
+	            public Class<?> getColumnClass(int columnIndex) {
+	                switch (columnIndex) {
+	                    case 0: return Integer.class;          // ID
+	                    case 1: return ImageIcon.class;        // Immagini
+	                    case 2: return String.class;           // Titolo dell'annuncio
+	                    case 3: return String.class;           // Descrizione
+	                    case 4: return String.class;           // Prezzo
+	                    default: return Object.class;
+	                }
+	            }
 
-    	        return true;
-    	    } catch (SQLException | IllegalArgumentException | ClassCastException e) {
-    	        e.printStackTrace();
-    	        return false;
-    	    }
-    	}
-     
-     
-     public Immobile recuperaDettagli(long idImmobile) {
-    	    try {
-    	        Connection connAWS = ConnessioneDatabase.getInstance().getConnection();
-    	        ImmobileDAO immobileDAO = new ImmobileDAO(connAWS);
-    	        return immobileDAO.getImmobileById(idImmobile);
-    	    } catch (SQLException e) {
-    	        e.printStackTrace();
-    	        return null;
-    	    }
-    	}
-
-     
-     
-
-     public int riempiTableRisultati(JTable tableRisultati, String campoPieno, String tipologia, Filtri filtri) {
-    	    try {
-    	        Connection connAWS = ConnessioneDatabase.getInstance().getConnection();
-    	        ImmobileDAO immobileDAO = new ImmobileDAO(connAWS);
-
-    	        NumberFormat format = NumberFormat.getNumberInstance(Locale.ITALY);
-    	        format.setGroupingUsed(true);
-    	        format.setMaximumFractionDigits(0);
-    	        format.setMinimumFractionDigits(0);
-
-    	        String[] colonne = { "ID", "Immagini", "Titolo dell'annuncio", "Descrizione", tipologia.equals("Vendita") ? "Prezzo Totale" : "Prezzo Mensile" };
-    	        @SuppressWarnings("serial")
-    	DefaultTableModel model = new DefaultTableModel(colonne, 0) {
-    	            @Override
-    	            public Class<?> getColumnClass(int columnIndex) {
-    	                switch (columnIndex) {
-    	                    case 0: return Integer.class;          // ID
-    	                    case 1: return ImageIcon.class;        // Immagini
-    	                    case 2: return String.class;           // Titolo dell'annuncio
-    	                    case 3: return String.class;           // Descrizione
-    	                    case 4: return String.class;           // Prezzo
-    	                    default: return Object.class;
-    	                }
-    	            }
-
-    	            @Override
-    	            public boolean isCellEditable(int row, int column) {
-    	                return false; // tutte le celle non editabili
-    	            }
-    	        };
+	            @Override
+	            public boolean isCellEditable(int row, int column) {
+	                return false; // tutte le celle non editabili
+	            }
+	        };
 
 
-    	        if (tipologia.equals("Affitto")) {
-    	            for (ImmobileInAffitto imm : immobileDAO.getImmobiliAffitto(campoPieno, filtri)) {
-    	            ImageIcon immagine = ImageUtils.decodeToIcon(imm.getIcon(), 60, 60);
-    	           
-    	   	            
-    	                model.addRow(new Object[] {
-    	                    imm.getId(),
-    	                    immagine,
-    	                    imm.getTitolo(),
-    	                    imm.getDescrizione(),
-    	                    "€ " + format.format(imm.getPrezzoMensile())
-    	                });
-    	            }
-    	        } else if (tipologia.equals("Vendita")) {
-    	            for (ImmobileInVendita imm : immobileDAO.getImmobiliVendita(campoPieno, filtri)) {
-    	            ImageIcon immagine = ImageUtils.decodeToIcon(imm.getIcon(), 60, 60);
-    	            
-    	            
-    	            
-    	            
-    	            
-    	                model.addRow(new Object[] {
-    	                    imm.getId(),
-    	                    immagine,
-    	                    imm.getTitolo(),
-    	                    imm.getDescrizione(),
-    	                    "€ " + format.format(imm.getPrezzoTotale())
-    	                });
-    	            }
-    	        }
+	        if (tipologia.equals("Affitto")) {
+	            for (ImmobileInAffitto imm : immobileDAO.getImmobiliAffitto(campoPieno, filtri)) {
+	            ImageIcon immagine = ImageUtils.decodeToIcon(imm.getIcon(), 60, 60);
+	           
+	   	            
+	                model.addRow(new Object[] {
+	                    imm.getId(),
+	                    immagine,
+	                    imm.getTitolo(),
+	                    imm.getDescrizione(),
+	                    "€ " + format.format(imm.getPrezzoMensile())
+	                });
+	            }
+	        } else if (tipologia.equals("Vendita")) {
+	            for (ImmobileInVendita imm : immobileDAO.getImmobiliVendita(campoPieno, filtri)) {
+	            ImageIcon immagine = ImageUtils.decodeToIcon(imm.getIcon(), 60, 60);
+	            
+	            
+	            
+	            
+	            
+	                model.addRow(new Object[] {
+	                    imm.getId(),
+	                    immagine,
+	                    imm.getTitolo(),
+	                    imm.getDescrizione(),
+	                    "€ " + format.format(imm.getPrezzoTotale())
+	                });
+	            }
+	        }
 
-    	        tableRisultati.setModel(model);
-    	        
+	        tableRisultati.setModel(model);
+	        
 
-    	        // Configura colonne
-    	        TableColumnModel columnModel = tableRisultati.getColumnModel();
-    	        columnModel.getColumn(0).setMinWidth(0);
-    	        columnModel.getColumn(0).setMaxWidth(0);
-    	        columnModel.getColumn(0).setWidth(0);
-    	        columnModel.getColumn(0).setPreferredWidth(0);
-    	        columnModel.getColumn(1).setPreferredWidth(150);
-    	        columnModel.getColumn(2).setPreferredWidth(170);
-    	        columnModel.getColumn(3).setPreferredWidth(450);
-    	        columnModel.getColumn(4).setPreferredWidth(75);
+	        // Configura colonne
+	        TableColumnModel columnModel = tableRisultati.getColumnModel();
+	        columnModel.getColumn(0).setMinWidth(0);
+	        columnModel.getColumn(0).setMaxWidth(0);
+	        columnModel.getColumn(0).setWidth(0);
+	        columnModel.getColumn(0).setPreferredWidth(0);
+	        columnModel.getColumn(1).setPreferredWidth(150);
+	        columnModel.getColumn(2).setPreferredWidth(170);
+	        columnModel.getColumn(3).setPreferredWidth(450);
+	        columnModel.getColumn(4).setPreferredWidth(75);
 
-    	        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
-    	        centerRenderer.setHorizontalAlignment(SwingConstants.CENTER);
-    	        columnModel.getColumn(4).setCellRenderer(centerRenderer);
+	        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+	        centerRenderer.setHorizontalAlignment(SwingConstants.CENTER);
+	        columnModel.getColumn(4).setCellRenderer(centerRenderer);
 
-    	        columnModel.getColumn(3).setCellRenderer(new TextAreaRenderer());
+	        columnModel.getColumn(3).setCellRenderer(new TextAreaRenderer());
 
-    	        tableRisultati.setRowHeight(100);
-    	        
-    	        
-    	        
-    	        // Imposto il render nella tabella, sulla colonna delle immagini 
-    	        TableUtils.setImageRenderer(tableRisultati, 1);
-    	        
-    	        return model.getRowCount();
-    	        
-    	      
-
+	        tableRisultati.setRowHeight(100);
+	        
+	        
+	        
+	        // Imposto il render nella tabella, sulla colonna delle immagini 
+	        TableUtils.setImageRenderer(tableRisultati, 1);
+	        
+	        return model.getRowCount();
     	    } catch (SQLException e) {
     	        e.printStackTrace();
     	    }
-    	return 0;
-    	}
-
-    	public void riempiTableRisultati(JTable tableRisultati) {
-    	 Connection connAWS;
-    	try {
-    	connAWS = ConnessioneDatabase.getInstance().getConnection();
-    	ImmobileDAO immobileDAO = new ImmobileDAO(connAWS);
-    	immobileDAO.riempiTableRisultatiDAO(tableRisultati);
-    	} catch (SQLException e) {
-    	// TODO Auto-generated catch block
-    	e.printStackTrace();
-    	}
     	    
-
-    	}
-
-    	// recupera le informazioni del profilo
-        public String[] getInfoProfilo(String emailUtente) throws SQLException {
-            try (Connection connAWS = ConnessioneDatabase.getInstance().getConnection()) {
-                AccountDAO accountDAO = new AccountDAO(connAWS);
-                return accountDAO.getInfoProfiloDAO(emailUtente);
-            }
-        }
-
-public String getIdSession(String email) throws SQLException{
-	 Connection connAWS = ConnessioneDatabase.getInstance().getConnection();
-	 AccountDAO accountDAO = new AccountDAO(connAWS);
-	 String idResult = "undef";
-	 
-	 // recupero dal db la stringa ottenuta (DAO)
-	 idResult = accountDAO.getSession(email);
-	 System.out.println("idResult: "+ idResult);
-	 
-	 return idResult;
-}
-
-
-public boolean InserisciOfferta(double offertaProposta, String idAccount, long idImmobile) {
-    try {
-        Connection connAWS = ConnessioneDatabase.getInstance().getConnection();
-        OffertaDAO offertaDAO = new OffertaDAO(connAWS);
-
-        Offerta offerta = new Offerta(offertaProposta, idAccount, idImmobile);  // CREA OGGETTO OFFERTA
-        return offertaDAO.inserisciOfferta(offerta);  // PASSA OGGETTO AL DAO
-
-    } catch (SQLException e) {
-        e.printStackTrace();
-        return false;
+    	    return 0;
     }
-}
 
-}
+ 	public void riempiTableRisultati(JTable tableRisultati) {
+	 	Connection connAWS;
+		try {
+			connAWS = ConnessioneDatabase.getInstance().getConnection();
+			ImmobileDAO immobileDAO = new ImmobileDAO(connAWS);
+			immobileDAO.riempiTableRisultatiDAO(tableRisultati);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			}
+ 	}
 
+    // recupera le informazioni del profilo
+    public String[] getInfoProfilo(String emailUtente) throws SQLException {
+        try (Connection connAWS = ConnessioneDatabase.getInstance().getConnection()) {
+            AccountDAO accountDAO = new AccountDAO(connAWS);
+            return accountDAO.getInfoProfiloDAO(emailUtente);
+            }
+    }
+
+    public String getIdSession(String email) throws SQLException{
+    	Connection connAWS = ConnessioneDatabase.getInstance().getConnection();
+    	AccountDAO accountDAO = new AccountDAO(connAWS);
+    	String idResult = "undef";
+	 
+    	// recupero dal db la stringa ottenuta (DAO)
+    	idResult = accountDAO.getSession(email);
+    	System.out.println("idResult: "+ idResult);
+	 
+    	return idResult;
+    }
+
+    public boolean InserisciOfferta(double offertaProposta, String idAccount, long idImmobile) {
+    	try {
+    		Connection connAWS = ConnessioneDatabase.getInstance().getConnection();
+    		OffertaDAO offertaDAO = new OffertaDAO(connAWS);
+
+	        Offerta offerta = new Offerta(offertaProposta, idAccount, idImmobile);  // CREA OGGETTO OFFERTA
+	        return offertaDAO.inserisciOfferta(offerta);  // PASSA OGGETTO AL DAO
 	
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	        return false;
+	    }
+    }
+
+}
