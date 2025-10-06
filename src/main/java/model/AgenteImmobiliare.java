d4e9cbd21abe0f4321554699b6e06b9e97b7989a
Manu2791 <borgiamanuele@gmail.com>
2025-10-06 12:25:09 +0200

    legato al precedente

--- src/main/java/model/AgenteImmobiliare.java
+++ src/main/java/model/AgenteImmobiliare.java
@@ -1,19 +1,18 @@
 package model;
 
 public class AgenteImmobiliare extends Account {
-    private String idAgente;
+    
     private String agenzia; 
 
-    public AgenteImmobiliare(String email, String password, String nome, String cognome, 
+    public AgenteImmobiliare(String id, String email, String password, String nome, String cognome, 
                              String citta, String telefono, String cap, String indirizzo, 
                              String ruolo, String agenzia) {
-        super(email, password, nome, cognome, citta, telefono, cap, indirizzo, ruolo);
+        super(id,email, password, nome, cognome, citta, telefono, cap, indirizzo,ruolo);
         this.agenzia = agenzia;
     }
 
     // getter e setter
-    public String getIdAgente() { return idAgente; }
-    public void setIdAgente(String idAgente) { this.idAgente = idAgente; }
+    
 
     public String getAgenzia() { return agenzia; }
     public void setAgenzia(String agenzia) { this.agenzia = agenzia; }
