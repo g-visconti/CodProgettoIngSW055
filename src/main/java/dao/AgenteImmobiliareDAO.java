28bd5a22165310161dcec18fe440c303ab1252bf
Manu2791 <borgiamanuele@gmail.com>
2025-10-06 12:34:11 +0200

    legata al precedente

--- src/main/java/dao/AgenteImmobiliareDAO.java
+++ src/main/java/dao/AgenteImmobiliareDAO.java
@@ -1,8 +1,11 @@
 package dao;
 
+import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.UUID;
 
 import model.AgenteImmobiliare;
 
@@ -14,9 +17,10 @@ public class AgenteImmobiliareDAO {
     }
 
     public void insertAgente(AgenteImmobiliare agente) throws SQLException {
-        String sql = "INSERT INTO \"AgenteImmobiliare\" (\"idAgente\", agenzia, \"email\") VALUES (?, ?, ?)";
+        // ID già presente, passato da Account
+        String sql = "INSERT INTO \"AgenteImmobiliare\" (id, agenzia, email) VALUES (?, ?, ?)";
         try (PreparedStatement ps = conn.prepareStatement(sql)) {
-            ps.setString(1, agente.getIdAgente());
+            ps.setString(1, agente.getIdAccount()); // ID generato dal DB in Account
             ps.setString(2, agente.getAgenzia());
             ps.setString(3, agente.getEmail());
             ps.executeUpdate();
